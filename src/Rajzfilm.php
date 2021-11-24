44af3ec4a718194af81cc7f8595295265624804a
Levente Ibrányi <ibranyilevente@gmail.com>
2021-11-24 12:41:20 +0100

    timestamps disable, guarded id

--- src/Rajzfilm.php
+++ src/Rajzfilm.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rajzfilm extends Model{
     protected $table = "rajzfilmek"; //ha angol lenne a db akkor nem lenne erre szükség
+    public $timestamps = false;
+    //protected $fillable = ['cim', 'hossz', 'kiadasi_ev'];
+    protected $guarded = ['id'];
 }
